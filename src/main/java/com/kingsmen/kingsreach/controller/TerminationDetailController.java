869a21edf8aab22b3a73ff6aa4a06079ac1a248f
meHarshh <meharshhk@gmail.com>
2024-11-20 16:13:00 +0530

    error resolved

--- src/main/java/com/kingsmen/kingsreach/controller/TerminationDetailController.java
+++ src/main/java/com/kingsmen/kingsreach/controller/TerminationDetailController.java
@@ -1,12 +1,10 @@
 package com.kingsmen.kingsreach.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind      .annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kingsmen.kingsreach.entity.TerminationDetail;
 import com.kingsmen.kingsreach.service.TerminationDetailService;
@@ -16,13 +14,13 @@ public class TerminationDetailController {
 
 	@Autowired
 	private TerminationDetailService terminationDetailService;
-	
-	@PostMapping(value ="/terminationDetail")
+
+	@PostMapping(value = "/terminationDetail")
 	private TerminationDetail terminationDetail(@RequestBody TerminationDetail detail) {
 		return terminationDetailService.terminationDetail(detail);
 	}
-	
-	@PutMapping("/editTerminationDetail")
+
+	@PutMapping(value = "/editTerminationDetail")
 	public TerminationDetail editTermination(@RequestBody TerminationDetail terminationDetail) {
 		return terminationDetailService.editTermination(terminationDetail);
 	}
