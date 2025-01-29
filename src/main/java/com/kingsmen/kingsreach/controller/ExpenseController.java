f392678679f2e996e0f586b262a90268f2c9edf6
bhoomikand <bhoomikand304@gmail.com>
2025-01-29 18:34:49 +0530

    changed

--- src/main/java/com/kingsmen/kingsreach/controller/ExpenseController.java
+++ src/main/java/com/kingsmen/kingsreach/controller/ExpenseController.java
@@ -16,7 +16,7 @@ import com.kingsmen.kingsreach.entity.Expense;
 import com.kingsmen.kingsreach.service.ExpenseService;
 import com.kingsmen.kingsreach.util.ResponseStructure;
 
-@CrossOrigin(allowCredentials = "true", origins = "http://hrms.kingsmenrealty.com/")
+@CrossOrigin(allowCredentials = "true", origins = "https://hrms.kingsmenrealty.com/")
 @RestController
 public class ExpenseController {
 
