2529253e90b43e3da9d072ad0c4f4124f07c863d
bhoomikand <bhoomikand304@gmail.com>
2025-03-11 14:59:57 +0530

    updated

--- src/main/java/com/kingsmen/kingsreach/serviceimpl/LeaveServiceImpl.java
+++ src/main/java/com/kingsmen/kingsreach/serviceimpl/LeaveServiceImpl.java
@@ -134,7 +134,7 @@ public class LeaveServiceImpl implements LeaveService {
 				int lopDays = (int) leaveDays - maxSickLeave;
 				payroll.setLopDays(payroll.getLopDays() + lopDays);
 			}
-		//	existingLeave.setSickLeaveBalance(existingLeave.getSickLeaveBalance() - (int) leaveDays);
+			existingLeave.setSickLeaveBalance(existingLeave.getSickLeaveBalance() - (int) leaveDays);
 			break;
 
 		case LeaveType.CASUAL:
@@ -146,7 +146,7 @@ public class LeaveServiceImpl implements LeaveService {
 				int lopDays = (int) leaveDays - maxCasualLeave;
 				payroll.setLopDays(payroll.getLopDays() + lopDays);
 			}
-		//	existingLeave.setCasualLeaveBalance(existingLeave.getCasualLeaveBalance() - (int) leaveDays);
+			existingLeave.setCasualLeaveBalance(existingLeave.getCasualLeaveBalance() - (int) leaveDays);
 			break;
 
 		case LeaveType.PAID:
@@ -162,7 +162,7 @@ public class LeaveServiceImpl implements LeaveService {
 				int lopDays = (int) leaveDays - maxPaidLeave;
 				payroll.setLopDays(payroll.getLopDays() + lopDays);
 			}
-		//	existingLeave.setPaidLeaveBalance(existingLeave.getPaidLeaveBalance() - (int) leaveDays);
+			existingLeave.setPaidLeaveBalance(existingLeave.getPaidLeaveBalance() - (int) leaveDays);
 			break;
 
 		case LeaveType.EMERGENCY:
@@ -174,7 +174,7 @@ public class LeaveServiceImpl implements LeaveService {
 				int lopDays = (int) leaveDays - existingLeave.getEmergencyLeaveBalance();
 				payroll.setLopDays(payroll.getLopDays() + lopDays);
 			}
-		//	existingLeave.setEmergencyLeaveBalance(existingLeave.getEmergencyLeaveBalance() - (int) leaveDays);
+			existingLeave.setEmergencyLeaveBalance(existingLeave.getEmergencyLeaveBalance() - (int) leaveDays);
 			break;
 
 		default:
